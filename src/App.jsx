4ea85dfe1fff49d8d6dde78baf9f22d19c580960
RaqeebDev <raqeebauto@gmail.com>
2026-02-18 09:31:27 +0530

    States Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import { useState } from "react";
 import { useEffect } from "react";
 
-import "./App.css";
+import "./styles.css";
 
-//Function for Random strings (Return random mail!)
+//return random mail
 function genLocalMail() {
   const user = Math.random().toString(36).slice(2, 10);
   return "{user}@mails.zepmail.xyz";
 }
 
 function App() {
-  const [count, setCount] = useState(0);
+  //States
+  const [theme, setTheme] = useState(() => localStorage.getItem("theme") || "dark");
+  const [mail, setMail] = useState(() => genLocalMail);
+  const [messages, setMessages] = useState([]);
+  const [active, setActive] = useState(null);
+
 
   return <div>Hey!</div>;
 }
