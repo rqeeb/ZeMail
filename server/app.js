ac05a0672e883b88b560410027aa3c8b9797b8c3
RaqeebDev <raqeebauto@gmail.com>
2026-02-22 09:53:30 +0530

    init app.js - base code

--- server/app.js
+++ server/app.js
@@ -1,5 +1,23 @@
 const express = require("express");
+const cors = require("cors")
 
+
+//Routes
 
 
 const app = express();
+const PORT = process.env.PORT || 2022;
+
+//Middlewares
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/" , (req,res) =>{
+    res.send("Server is Running gng.");
+    console.log("Hit!");
+})
+
+app.listen(PORT, () =>{
+    console.log("it's running!");
+});
