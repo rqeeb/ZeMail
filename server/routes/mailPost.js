6ce3d1bcb3969448c4746f062b14f11c541bdc0e
RaqeebDev <raqeebauto@gmail.com>
2026-02-25 21:25:37 +0530

    testing if render works after commenting out mailPost

--- server/routes/mailPost.js
+++ server/routes/mailPost.js
@@ -3,28 +3,28 @@ const app = express();
 const mailPost = express.Router();
 const mailModel = require("../db");
 
-mailPost.post("/post", (req, res) => {
-  const email = req.email;
-  const time = req.time;
+// mailPost.post("/post", (req, res) => {
+//   const email = req.email;
+//   const time = req.time;
   
-  try {
-    await mailModel.create({
-      email:email,
-      time:time,
-    })
-    .then(() => {
-      console.log("111");
-    })
-  }
-  catch(err){
-    console.log(`Error: ${err}`);
-  }
+//   try {
+//     await mailModel.create({
+//       email:email,
+//       time:time,
+//     })
+//     .then(() => {
+//       console.log("111");
+//     })
+//   }
+//   catch(err){
+//     console.log(`Error: ${err}`);
+//   }
 
  
 
   //make mongo , send to db , delete if pressed refresh/after 40 mins,  
-});
+})
 
-module.exports = {
-  mailPost: mailPost,
-};
+// module.exports = {
+//   mailPost: mailPost,
+// };
