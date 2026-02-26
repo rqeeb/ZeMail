a83724367735c6ee2b625d2b19326e50b9177630
RaqeebDev <raqeebauto@gmail.com>
2026-02-26 20:00:34 +0530

    db and package.json

--- server/db.js
+++ server/db.js
@@ -1,35 +1,9 @@
 const mongoose = require("mongoose");
-const { Schema } = mongoose;
-require("dotenv").config();
 
-const mongoURI = process.env.MONGODB_URI;
-
-async function connectDb() {
-  await mongoose
-    .connect(mongoURI)
-    .then(() => {
-      console.log("Mongo connected");
-    })
-    .catch((err) => {
-      console.log("Connection error:", err);
-    });
+async function connectDB(uri) {
+  mongoose.set("strictQuery", true);
+  await mongoose.connect(uri);
+  console.log("DB connected");
 }
 
-// Schema
-const mailSchema = new Schema({
-  email: {
-    type: String,
-    required: true,
-  },
-  time: {
-    type: Date,
-    default: Date.now,
-  },
-});
-
-const mailModel = mongoose.model("Mail", mailSchema);
-
-module.exports = {
-  mailModel,
-  connectDb,
-};
+module.exports = { connectDB };
